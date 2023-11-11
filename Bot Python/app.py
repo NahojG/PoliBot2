f19ba5d0d6227353b1519901844cbb631a60836d
NahojG <johan@granados.com.co>
2023-11-11 11:23:07 -0500

    a free language model is changed to gpt 3.5

--- Bot Python/app.py
+++ Bot Python/app.py
@@ -1,27 +1,33 @@
 
-from flask import Flask, request, jsonify, render_template
-from transformers import AutoModelForCausalLM, AutoTokenizer
+import openai
 from flask_cors import CORS
+from flask import Flask, request, jsonify, render_template
 
-app = Flask(__name__, static_folder='/Users/nahojgranados/Downloads/static', template_folder='/Users/nahojgranados/Downloads/templates')
+app = Flask(__name__)
 CORS(app)
 
-# Load your model and tokenizer
-tokenizer = AutoTokenizer.from_pretrained('EleutherAI/gpt-neo-125M')
-model = AutoModelForCausalLM.from_pretrained('EleutherAI/gpt-neo-125M')
+# Asegúrate de configurar tu clave API de OpenAI como una variable de entorno o directamente aquí
+openai.api_key = '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'
 
 @app.route('/')
 def home():
-    return render_template('chat.html')  # Ensure your HTML file is named chat.html
+    return render_template('chat.html')
 
 @app.route('/ask', methods=['POST'])
 def ask():
     data = request.json
     input_text = data['question']
-    input_ids = tokenizer.encode(input_text, return_tensors='pt')
-    output = model.generate(input_ids, max_length=200)
-    response_text = str(tokenizer.decode(output[0], skip_special_tokens=True))
+    
+    # Hacer una solicitud al API de OpenAI usando el modelo gpt-3.5-turbo
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "system", "content": "You are a helpful assistant."},
+                  {"role": "user", "content": input_text}]
+    )
+    
+    # Obtén la respuesta generada
+    response_text = response['choices'][0]['message']['content']
     return jsonify(response=response_text)
 
 if __name__ == '__main__':
-    app.run(port=5000)  # Listen on TCP port 5000
+    app.run(port=5000)
